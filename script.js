0e638e5e7e8eedcbeff5963bed4a4432e1d02b5b
jafferytech110 <sarfarazalijaffery@gmail.com>
2022-03-19 22:57:19 +0100

    all done

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+// computing of interest
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
+{   
+    // declaring varialbles and taking values 
+   
+    let principal = document.getElementById('principal').value;
+    if(principal <= 0) {
+        alert('please enter a positive value of principal amount');
+        document.getElementById('principal').focus();
+    }
+    let rate = document.getElementById('rate').value;
+    let years = parseInt(document.getElementById('years').value);
+    console.log(years)
+    let interest = principal * years * (rate /100);
+    var year = new Date().getFullYear()+ years;
+    console.log(year)
+    let total = parseInt(interest);
+    // showing result in span tag
+    document.getElementById('result').innerText = 
+    `if you deposit ${principal},
+    at an interest rate of ${rate}%.
+    You will receive an amount including principal amount ${total} ,
+    in the year ${year}`;    
 }
-        
+// updating rate function
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}    
